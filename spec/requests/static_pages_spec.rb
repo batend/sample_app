8d0f35fa5a8b7f6513c02853a3b6b3117d145aaa
batend <benniu.618@gmail.com>
2014-03-05 13:18:50 -0600

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,42 +2,34 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  subject {page}
+
   describe "Home page" do
-	  it "should have the content 'Sample App'" do
-		  visit '/static_pages/home'
-		  expect(page).to have_content('Sample App')
-    end
+    before {visit root_path}
 
-    it "should have the base title" do
-    	visit '/static_pages/home'
-    	expect(page).to have_title("Jing AND Ben")
-    end
-
-    # it "should not have a custom page title" do
-    #   visit '/static_pages/home'
-    #   expect(page).not_to have_title('| Home')
-    # end
-      
+    it {should have_content('Sweet Home')}
+    it {should have_title('Jing AND Ben')}
   end
 
   describe "Help page" do
-  	it "should have the content 'Help'" do
-  		visit '/static_pages/help'
-  		expect(page).to have_content('Help')
-  	end
+    before{visit help_path}
 
-  	it "should have the right title" do
-  		visit '/static_pages/help'
-  		expect(page).to have_title("Jing AND Ben | Help")
-  	end
-
+    it {should have_content('help')}
+    it {should have_title('Jing AND Ben | Help')}
   end
 
   describe "About page" do
-  	it "shoud have the content 'About'" do
-  		visit '/static_pages/about'
-  		expect(page).to have_content('About')
-  	end
+    before {visit about_path}
+
+    it {should have_content('About')}
   end
+
+  describe "Contact page" do
+    before {visit contact_path}
+
+    it {should have_content('Contact')}
+    it {should have_title('Jing AND Ben | Contact')}
+  end
+
 
 end
